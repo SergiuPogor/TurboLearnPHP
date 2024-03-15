988f0894daf2a3c205eca37e9056ceff5befb47e
Serghei Pogor <serghei.pogor@stanga.net>
2024-03-15 09:44:36 +0200

    added multidimensional_array

---
+++ multidimensional_array.php
@@ -0,0 +1,29 @@
+<?php
+
+$data = [
+    'name' => 'Mike Smith',
+    'contacts' => [
+        'emails' => [
+            'mk1@gmail.com',
+            'mk2@gmail.com',
+            'mk3@gmail.com',
+        ],
+        'phone' => '123-456-7890'
+    ],
+    'tags' => ['php', 'developer', 'backend'],
+    'details' => [
+        'projects' => [
+            'project1' => 'Library Management System',
+            'project2' => 'Online Action System'
+        ]
+    ]
+];
+
+
+array_walk_recursive($data, function (&$item, $key) {
+    if (is_string($item)) {
+        $item = strtoupper($item);
+    }
+});
+
+var_dump($data);
