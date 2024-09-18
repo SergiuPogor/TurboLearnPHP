da3abb68419be4a52407c44beb93a69e5db86ab3
Serghei Pogor <serghei.pogor@stanga.net>
2024-09-18 17:37:18 +0300

    Dynamic hash generation using random algorithms with fallback for mhash absence.

--- examples/videos/5508.php
+++ examples/videos/5508.php
@@ -1,25 +1,60 @@
 <?php
 
-// Check if mhash extension is loaded
-if (!extension_loaded('mhash')) {
-    die('The mhash extension is not available.');
+// Dynamic hash generation using random algorithms with fallback for mhash absence.
+
+function getHashAlgorithmNameOrFallback($algorithmId)
+{
+    if (extension_loaded('mhash')) {
+        return mhash_get_hash_name($algorithmId);
+    } else {
+        $legacyFallbacks = [
+            MHASH_MD5 => 'md5',
+            MHASH_SHA256 => 'sha256',
+            MHASH_SHA1 => 'sha1',
+        ];
+        return $legacyFallbacks[$algorithmId] ?? 'md5';
+    }
 }
 
-// Get the hash identifier for a specific algorithm
-$hashId = MHASH_SHA256;
+function pickRandomAlgorithm()
+{
+    $algorithms = [
+        MHASH_MD5,
+        MHASH_SHA256,
+        MHASH_SHA1,
+        MHASH_RIPEMD160,
+        MHASH_TIGER,
+        MHASH_CRC32,
+    ];
+    return $algorithms[array_rand($algorithms)];
+}
 
-// Get the name of the hash algorithm from the identifier
-$hashName = mhash_get_hash_name($hashId);
+$chosenAlgorithm = pickRandomAlgorithm();
+$hashName = getHashAlgorithmNameOrFallback($chosenAlgorithm);
 
-// Output the hash algorithm name
-echo "Hash Algorithm Name: " . $hashName . PHP_EOL;
+echo "You've summoned the legendary Hash Algorithm: $hashName!" . PHP_EOL;
 
-// Example of using the hash algorithm name in a hash operation
-$data = 'Sample data for hashing';
-$hash = mhash($hashId, $data);
+$dataArray = [
+    'Unicorn tears',
+    'PHP is love, PHP is life',
+    'Why did the hash cross the road?',
+    'Hashing ain’t easy, but somebody’s gotta do it',
+    'Just a random string for randomness',
+];
 
-// Display the hash value and name
-echo "Hash Value: " . bin2hex($hash) . PHP_EOL;
-echo "Algorithm Used: " . $hashName . PHP_EOL;
+$dataToHash = $dataArray[array_rand($dataArray)];
 
-?>
+echo "Selected data for hashing: '$dataToHash'" . PHP_EOL;
+
+if (extension_loaded('mhash')) {
+    $hash = mhash($chosenAlgorithm, $dataToHash);
+} else {
+    $hash = hash($hashName, $dataToHash);
+}
+
+echo "And the mighty hash value is: " . bin2hex($hash) . PHP_EOL;
+
+$comparison = bin2hex($hash) === hash('md5', 'I love PHP') ? 'MATCHED!' : 'TOTALLY DIFFERENT!';
+
+echo "Does our hash match the sacred 'I love PHP' hash? $comparison" . PHP_EOL;
+
