39db3ed61983ef363794f2bc698c747e046673d3
Sergiu Pogor <mediapronetsrl@gmail.com>
2024-10-28 11:44:50 +0200

    This is how you set PHP settings in .htaccess

---
+++ examples/shorts/9150.php
@@ -0,0 +1,16 @@
+<?php
+// This is how you set PHP settings in .htaccess
+// Note: Use this approach only if your server supports .htaccess overrides
+
+// To increase memory limit
+// Add this line to your .htaccess file
+// php_value memory_limit 128M
+
+// To display all errors
+// php_flag display_errors On
+
+// To set a specific upload limit
+// php_value upload_max_filesize 10M
+
+// Make sure to restart your server if needed
+// for changes to take effect
