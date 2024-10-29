3a339d902a98f613e81020bb6c86cfe96a46f4fb
Sergiu Pogor <mediapronetsrl@gmail.com>
2024-10-29 11:28:46 +0200

    This function displays the PHP configuration settings

---
+++ examples/shorts/9237.php
@@ -0,0 +1,9 @@
+<?php
+// This function displays the PHP configuration settings
+function displayPhpInfo() {
+    // Output PHP information
+    phpinfo();
+}
+
+// Call the function to display info
+displayPhpInfo();
